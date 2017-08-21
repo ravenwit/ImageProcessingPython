4c9c15dc16be15e684abdd81aad4c9ffe8d75cba
Shakir Ahmed <s.ahmed7733@gmail.com>
2017-08-21 07:37:33 +0600

    Haar Cascade updated

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,29 +1,71 @@
+#!/usr/bin/env python
+
+'''
+face detection using haar cascades
+
+USAGE:
+    facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
 import cv2
 
-face_casacde = cv2.CascadeClassifier('face.xml')
+# local modules
+from common import clock, draw_str
 
-eye_cascade = cv2.CascadeClassifier('eye.xml')
 
-cap = cv2.VideoCapture(0)
-# cap.set(4, 400)
-# cap.set(5, 200)
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+                                     flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
 
-while True:
-    _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_casacde.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        rec_gray = gray[y:y+h, x:x+w]
-        rec_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(rec_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(rec_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
-    cv2.imshow('Video', frame)
+if __name__ == '__main__':
+    import sys, getopt
+    print(__doc__)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
+    try:
+        video_src = video_src[0]
+    except:
+        video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "./haarcascade_frontalface.xml")
+    nested_fn  = args.get('--nested-cascade', "./haarcascade_eye.xml")
 
-cap.release()
-cv2.destroyAllWindows()
+    cascade = cv2.CascadeClassifier(cascade_fn)
+    nested = cv2.CascadeClassifier(nested_fn)
+
+    cam = cv2.VideoCapture(video_src)
+
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        t = clock()
+        rects = detect(gray, cascade)
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+        if not nested.empty():
+            for x1, y1, x2, y2 in rects:
+                roi = gray[y1:y2, x1:x2]
+                vis_roi = vis[y1:y2, x1:x2]
+                subrects = detect(roi.copy(), nested)
+                draw_rects(vis_roi, subrects, (255, 0, 0))
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+
+        if cv2.waitKey(5) == 27:
+            break
+    cv2.destroyAllWindows()
